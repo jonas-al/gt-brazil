5432196dd3eeef0921598ef88c547d5a4e244a30
jonas-al <yonas.alluniverse@gmail.com>
2023-05-14 18:39:26 -0300

    feat: new project settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import paths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), paths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      src: path.resolve(__dirname, 'src/'),
+      components: path.resolve(__dirname, 'src/components/'),
+      pages: path.resolve(__dirname, 'src/pages/')
+    }
+  }
 })
