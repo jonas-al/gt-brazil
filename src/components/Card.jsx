49da4ccceb24fe85502f9a67f6d3545654c763a6
jonas-al <yonas.alluniverse@gmail.com>
2023-06-16 12:32:55 -0300

    refact: changes in the arrangement of information

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,14 +6,17 @@ const Card = ({ group, handleViewGroupDetails }) => {
       <div className='min-w-[130px] bg-[#C4594B]' />
       <div className='p-4'>
         <button
-          className='text-2xl text-start'
+          className='text-2xl text-start hover:underline'
           onClick={() => handleViewGroupDetails(group)}
         >
           {group.name}
         </button>
-        {group.local.map((elem, index) => (
-          <p key={index} className='text-xl font-light'>{elem}</p>
-        ))}
+        <div className='flex gap-x-2'>
+          {group.local.map((elem, index, list) => {
+            if (index + 1 !== list.length) return <p key={index} className='text-xl font-light'>{elem},</p>
+            return <p key={index} className='text-xl font-light'>{elem}</p> 
+          })}
+        </div>
       </div>
     </div>
   )
