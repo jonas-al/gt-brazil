d5e19b0d24666dae5235217ca90429fe99d3dd7e
jonas-al <yonas.alluniverse@gmail.com>
2023-06-22 12:05:41 -0300

    feat: section about

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,30 @@ function App() {
   }
 
   return (
-    <div className="w-screen bg-gray-50">
-      <img src={shape} alt="Shape" className='absolute right-[-100px]'/>
-      <header className='flex w-full justify-between px-8 pt-4'>
-        <div className='flex items-center justify-center gap-x-4'>
-          <img src={logo} alt="Logo" />
-          <h1 className='text-3xl text-purple-950 font-bold'>Grupos de Trabalho</h1>
-        </div>
-        <nav className='flex gap-x-4 z-10'>
-          <a href="/" className='text-xl'>Home</a>
-          <p>|</p>
-          <a href="/" className='text-xl'>Mapa</a>
-          <p>|</p>
-          <a href="/" className='text-xl'>Contatos</a>
-        </nav>
-      </header>
+    <div className="bg-gray-50">
+      <div className='flex flex-col w-full h-screen gap-y-48'>
+        <img src={shape} alt="Shape" className='absolute w-[55%] right-1'/>
+        <header className='flex w-full justify-between pr-36 pt-4 pl-14'>
+          <div className='flex items-center justify-center gap-x-4'>
+            <img src={logo} alt="Logo" className='w-14'/>
+            <h1 className='text-3xl text-[#75233D] font-bold'>Grupos de Trabalho</h1>
+          </div>
+          <nav className='flex gap-x-4 z-10 text-xl text-white items-center justify-center'>
+            <button className='rounded p-1 hover:bg-white hover:text-[#4B1623]'>Home</button>
+            <p>|</p>
+            <button className='rounded p-1 hover:bg-white hover:text-[#4B1623]'>Mapa</button>
+            <p>|</p>
+            <button className='rounded p-1 hover:bg-white hover:text-[#4B1623]'>Contatos</button>
+          </nav>
+        </header>
+        <section className='w-[555px] h-full font-montserrat text-xl text-[#A8A8A8] pl-20'>
+          <p>Grupos de</p>
+          <h1 className='font-berkshire-swash text-6xl text-[#272727]'>Estudo & Pesquisa</h1>
+          <p className='text-justify'>
+            Phasellus pellentesque lacinia arcu sit amet egestas. Cras rhoncus ut orci vel efficitur. Nulla convallis, purus id mollis rhoncus, magna quam rutrum lacus, nec mollis.
+          </p>
+        </section>
+      </div>
       <div className='flex'>
         {showGroupDetails ? <GroupDatails group={selectedGroup} hideGroupDetails={hideGroupDetails} /> : <BrazilMap handleViewGroupDetails={handleViewGroupDetails} />}
       </div>
