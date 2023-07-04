5afb07766af50bd93c77d766c65986436395dee7
jonas-al <yonas.alluniverse@gmail.com>
2023-07-04 11:37:51 -0300

    feat: routing initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from 'pages/Home'
+import GroupDatails from 'pages/GroupDatails'
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/detalhes',
+    element: <GroupDatails />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
